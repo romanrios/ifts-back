746d2414dbb37f9da3dc0645454917023700fa72
Román Ríos <romanrios@live.com>
2025-09-04 20:26:23 -0300

    limpiar index.js

--- proyecto-en-clase/index.js
+++ proyecto-en-clase/index.js
@@ -1,9 +1,5 @@
 const express = require("express");
-const temario = require("./data/temario");
-const { crearTemario } = require('./controllers/temarioController');
-
 const temarioRoutes = require('./routes/temarioRoutes');
-
 const app = express();
 const PORT = 3000;
 
@@ -11,13 +7,6 @@ app.use(express.json());
 
 // Rutas
 app.use("/temario", temarioRoutes);
-
-
-// app.get("/temario", (req, res) => {
-//     res.json(temario);
-// });
-
-// app.post("/agregar", crearTemario);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
