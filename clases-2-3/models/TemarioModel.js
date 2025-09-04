78fb2770f594346bfa1d17baa05a4cde2112cf86
Román Ríos <romanrios@live.com>
2025-09-04 20:24:02 -0300

    clase 4 (4-9)

--- clases-2-3/models/TemarioModel.js
+++
@@ -1,9 +0,0 @@
-class Temario {
-    constructor(id, titulo, descripcion) {
-        this.id = id;
-        this.titulo = titulo;
-        this.descripcion = descripcion;
-    }
-}
-
-module.exports = Temario;
--- proyecto-en-clase/index.js
+++ proyecto-en-clase/index.js
@@ -2,16 +2,22 @@ const express = require("express");
 const temario = require("./data/temario");
 const { crearTemario } = require('./controllers/temarioController');
 
+const temarioRoutes = require('./routes/temarioRoutes');
+
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-app.get("/temario", (req, res) => {
-    res.json(temario);
-});
+// Rutas
+app.use("/temario", temarioRoutes);
 
-app.post("/agregar", crearTemario);
+
+// app.get("/temario", (req, res) => {
+//     res.json(temario);
+// });
+
+// app.post("/agregar", crearTemario);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
